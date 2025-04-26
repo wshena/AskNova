112aeb598d3803b5d35a5e83814634e650591262
poppywydy <adeliasekar7@gmail.com>
2025-04-26 14:28:14 +0700

    Add metadata on layout.tsx file.

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,4 +1,15 @@
+import { Metadata } from 'next'
 import React from 'react'
+
+export const metadata: Metadata = {
+  title: 'AskNova',
+  description: 'AskNova helps you get answers, find inspiration and be more productive. It is free to use and easy to try. Just ask and AskNova can help with writing, learning, brainstorming and more.',
+  keywords: 'ai chat,ai,chap gpt,chat gbt,chat gpt 3,chat gpt login,chat gpt website,chat gpt,chat gtp,chat openai,chat,chatai,chatbot gpt,chatg,chatgpt login,chatgpt,gpt chat,open ai,openai chat,openai chatgpt,openai',
+  openGraph: {
+    title: 'AskNova',
+    description: 'A conversational AI system that listens, learns, and challenges'
+  }
+}
 
 const layout = ({children}:{children:React.ReactNode}) => {
   return (
